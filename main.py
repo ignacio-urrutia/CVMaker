57367e40cfb2f9372901f8cfc4f013871b3469b4
Ignacio Urrutia Gajardo <ignacio.ug99@gmail.com>
2024-03-06 17:31:38 -0300

    Style Improvement

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ def json_to_html(json_file):
         'default': 'fa-solid fa-circle-user'
     }
 
+    contant_links = {
+        'phone': 'tel:',
+        'email': 'mailto:',
+        'website': 'https://www.',
+        'linkedin': 'https://www.linkedin.com/in/',
+        'github': 'https://www.github.com/',
+        'twitter': 'https://www.twitter.com/',
+        'location': ''
+    }
 
     with open(json_file, 'r') as file:
         cv_data = json.load(file)
@@ -27,14 +36,32 @@ def json_to_html(json_file):
         <style>
             body {{ font-family: Arial, sans-serif; margin: 40px; }}
             .section {{ margin-bottom: 20px; }}
-            .cv-title {{ font-size: 50px; margin-bottom: 20px; font-weight: bold; text-transform: uppercase; }}
+            .cv-title {{ font-size: 50px; margin-bottom: 20px; font-weight: bolder; text-transform: uppercase; }}
             .cv-subtitle {{ font-size: 24px; margin-bottom: 20px; font-weight: bold; }}
             .cv-header {{ margin-bottom: 40px; text-align: center; }}
-            .section-title {{ font-size: 24px; margin-bottom: 10px; }}
+            .section-title {{ font-size: 24px; margin-bottom: 10px; font-weight: bold; text-transform: uppercase; }}
             .contact-info {{ display: flex; flex-direction: row; justify-content: space-around; }}
+            
+            .experience_item_header {{ display: flex; justify-content: space-between; }}
+            .experience_position {{ font-size: 20px; font-weight: bold; }}
+            .experience_company {{ font-size: 18px; }}
+
             .item {{ margin-bottom: 5px; }}
             .details {{ margin-left: 20px; }}
             .details li {{ margin-bottom: 5px; }}
+
+            .progress {{ height: 10px; background-color: #d1d1d1; width: 200px; border-radius: 5px; }}
+            .progress-bar {{ background-color: #3C409F; height: 10px; border-radius: 5px; }}
+
+            .skills-list {{ display: flex; list-style: none;  flex-direction: row; flex-wrap: wrap; padding: 0; }}
+            .skills-list li {{ margin-right: 20px; margin-bottom: 20px; }}
+
+            ul {{ padding: 0; }}
+
+
+            @media print {{
+                body {{-webkit-print-color-adjust: exact;}}
+            }}
         </style>
         <script src="https://kit.fontawesome.com/265b36fe9b.js" crossorigin="anonymous"></script>
     </head>
@@ -45,40 +72,68 @@ def json_to_html(json_file):
         </div>
 
         <div class="contact-info section">
-            {''.join(f"<div class='item'><i class='fas {contact_icons.get(contact, contact_icons['default'])}'></i> {cv_data['contact_info'][contact]}</div>" for contact in cv_data['contact_info'])}
+            {''.join(f"""
+                     <div class='item'>
+                        <i class='fas {contact_icons.get(contact, contact_icons['default'])}'></i>
+                        <a href='{contant_links.get(contact, '')}{cv_data['contact_info'][contact]}'>{cv_data['contact_info'][contact]}</a>
+                    </div>
+                     """ for contact in cv_data['contact_info'])}
         </div>
 
         <div class="section">
             <div class="section-title">Summary</div>
+            <hr>
             <p>{cv_data['summary']}</p>
         </div>
 
         <div class="section">
             <div class="section-title">Experience</div>
-            {''.join(f"<div class='item'><b>{exp['position']}</b> at {exp['company']} ({exp['from']} - {exp['to']})<ul class='details'>{''.join(f"<li>{detail}</li>" for detail in exp['details'])}</ul></div>" for exp in cv_data['experience'])}
+            <hr>
+            {''.join(f"""
+                     <div class='item'>
+                        <div class='experience_item_header'>
+                            <div>
+                                <div class='experience_position'>{exp['position']}</div>
+                                <div class='experience_company'>{exp['company']}</div> 
+                            </div>
+                            <div class='experience_dates'>{exp['from']} - {exp['to']}</div>
+                        </div>
+                        <ul class='details'>{''.join(f"<li>{detail}</li>" for detail in exp['details'])}</ul></div>
+                     """ for exp in cv_data['experience'])}
         </div>
 
         <div class="section">
             <div class="section-title">Education</div>
+            <hr>
             {''.join(f"""
-                     <div class='item'><b>{edu['degree']}</b>, {edu['institution']} ({edu['from']} - {edu['to']})</div>
+                     <div class='item'>
+                        <b>{edu['degree']}</b>, {edu['institution']} ({edu['from']} - {edu['to']})</div>
                      """ for edu in cv_data['education'])}
         </div>
 
         <div class="section">
             <div class="section-title">Projects</div>
+            <hr>
             {''.join(f"<div class='item'><b>{proj['name']}</b>: {proj['description']} - <a href='{proj['link']}'>Link</a> (Technologies: {', '.join(proj['technologies'])})</div>" for proj in cv_data['projects'])}
         </div>
 
         <div class="section">
             <div class="section-title">Skills</div>
-            <ul class="details">
-                {''.join(f"<li>{skill}</li>" for skill in cv_data['skills'])}
+            <hr>
+            <ul class="skills-list">
+                {''.join(f"""
+                            <li>{skill}
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: {value}%" aria-valuenow="{value}" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </li>
+                         """ for skill, value in cv_data['skills'].items() if value)}
             </ul>
         </div>
 
         <div class="section">
             <div class="section-title">Certifications</div>
+            <hr>
             {''.join(f"<div class='item'>{cert['name']} (Issuer: {cert['issuer']}, Date: {cert['date']})</div>" for cert in cv_data['certifications'])}
         </div>
 
